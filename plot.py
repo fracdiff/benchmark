57b4a4ae7c9fd33620d0eca809915bd7ba67d8af
Shota Imaki <shota.imaki.0801@gmail.com>
2021-08-09 11:27:31 +0900

    develop

---
+++ plot.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import seaborn
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+GITHUB_GRAY = "#6a737d"
+GITHUB_BLUE = "#0366d6"
+GITHUB_ORANGE = "#f66a0a"
+CYCLE = matplotlib.cycler(color=[GITHUB_BLUE, GITHUB_ORANGE])
+
+seaborn.set_style("whitegrid")
+
+FONTSIZE = 18
+matplotlib.rcParams["axes.labelsize"] = FONTSIZE
+matplotlib.rcParams["axes.titlesize"] = FONTSIZE
+matplotlib.rcParams["figure.titlesize"] = FONTSIZE
+matplotlib.rcParams["legend.fontsize"] = FONTSIZE
+matplotlib.rcParams["xtick.labelsize"] = FONTSIZE
+matplotlib.rcParams["ytick.labelsize"] = FONTSIZE
+matplotlib.rcParams["axes.prop_cycle"] = CYCLE
+matplotlib.rcParams["grid.color"] = GITHUB_GRAY
+matplotlib.rcParams["axes.edgecolor"] = GITHUB_GRAY
+matplotlib.rcParams["legend.edgecolor"] = GITHUB_GRAY
+matplotlib.rcParams["axes.linewidth"] = 1.6
+matplotlib.rcParams["figure.dpi"] = 300
+matplotlib.rcParams["figure.figsize"] = (10, 5)
+matplotlib.rcParams["font.family"] = "sans-serif"
+matplotlib.rcParams["lines.linewidth"] = 2
+matplotlib.rcParams["savefig.bbox"] = "tight"
+matplotlib.rcParams["savefig.pad_inches"] = 0.1
+
+
+if __name__ == "__main__":
+    df_all = pd.read_csv("out/results.csv")
+
+    # Create markdown table
+    def fmt(x: float) -> str:
+        return f"{x:.3f}"
+
+    df = df_all[df_all["n_features"] == 1].copy()
+    series_time_mean = (1000 * df["time_mean"]).map(fmt)
+    series_time_std = (1000 * df["time_std"]).map(fmt)
+    df["time (ms)"] = series_time_mean.str.cat(series_time_std, sep=" +-")
+    with open("out/table_n_samples.md", "w") as f:
+        md = df.pivot("n_samples", columns=["method"], values="time (ms)").to_markdown()
+        f.write(md)
+
+    df = df_all[df_all["n_samples"] == 1000].copy()
+    series_time_mean = (1000 * df["time_mean"]).map(fmt)
+    series_time_std = (1000 * df["time_std"]).map(fmt)
+    df["time (ms)"] = series_time_mean.str.cat(series_time_std, sep=" +-")
+    with open("out/table_n_features.md", "w") as f:
+        md = df.pivot(
+            "n_features", columns=["method"], values="time (ms)"
+        ).to_markdown()
+        f.write(md)
+
+    # Plot
+
+    plt.figure(figsize=(16, 4))
+
+    plt.subplot(1,2,1)
+    df = df_all[df_all["n_features"] == 1]
+    dff = df[df["method"] == "fracdiff"]
+    dfo = df[df["method"] == "official"]
+    time_mean_ms_f = 1000 * dff["time_mean"]
+    time_std_ms_f = 1000 * dff["time_std"]
+    time_mean_ms_o = 1000 * dfo["time_mean"]
+    time_std_ms_o = 1000 * dfo["time_std"]
+    plt.errorbar(dff["n_samples"], time_mean_ms_f, yerr=time_std_ms_f, label="fracdiff", fmt="o")
+    plt.errorbar(dfo["n_samples"], time_mean_ms_o, yerr=time_std_ms_o, label="official", fmt="o")
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.xlabel("Number of Samples")
+    plt.ylabel("Computation Time [ms]")
+    plt.legend()
+    plt.title("Computation Time")
+
+    plt.subplot(1,2,2)
+    df = df_all[df_all["n_samples"] == 1000]
+    dff = df[df["method"] == "fracdiff"]
+    dfo = df[df["method"] == "official"]
+    time_mean_ms_f = 1000 * dff["time_mean"]
+    time_std_ms_f = 1000 * dff["time_std"]
+    time_mean_ms_o = 1000 * dfo["time_mean"]
+    time_std_ms_o = 1000 * dfo["time_std"]
+    plt.errorbar(dff["n_features"], time_mean_ms_f, yerr=time_std_ms_f, label="fracdiff", fmt="o")
+    plt.errorbar(dfo["n_features"], time_mean_ms_o, yerr=time_std_ms_o, label="official", fmt="o")
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.xlabel("Number of Features")
+    plt.ylabel("Computation Time [ms]")
+    plt.legend()
+    plt.title("Computation Time")
+
+    plt.savefig("out/time.png", bbox_inches="tight")
---
+++ time.py
@@ -0,0 +1,82 @@
+import time
+from itertools import product
+
+import fracdiff
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+from src.fracdiff_official import fracDiff_FFD
+
+
+def generate_dataframe(
+    n_samples: int, n_features: int, seed: int = None
+) -> pd.DataFrame:
+    if seed is not None:
+        np.random.seed(seed)
+    return pd.DataFrame(np.random.randn(n_samples, n_features).cumsum(0))
+
+
+def timeit(f, n_iter=1, **kwargs) -> tuple:
+    times = []
+    for _ in range(n_iter):
+        t = time.time()
+        f(**kwargs)
+        times.append(time.time() - t)
+    return np.mean(times), np.std(times)
+
+
+def time_fracdiff(
+    method: str,
+    n_samples: int,
+    n_features: int,
+    n: float = 0.5,
+    window: int = 10,
+    n_iter: int = 10,
+    seed: int = 42,
+) -> tuple:
+    a = generate_dataframe(n_samples=n_samples, n_features=n_features, seed=seed)
+    if method == "fracdiff":
+        f = fracdiff.fdiff
+    if method == "official":
+        f = fracDiff_FFD
+
+    return timeit(f, a=a, n_iter=n_iter, n=n, window=window)
+
+
+if __name__ == "__main__":
+    results = []
+
+    params_methods = ["fracdiff", "official"]
+
+    params_n_samples = [100, 1000, 10000, 100000]
+    params = product(params_n_samples, params_methods)
+    for n_samples, method in tqdm(list(params)):
+        n_features = 1
+        time_mean, time_std = time_fracdiff(method, n_samples, n_features)
+        results.append(
+            {
+                "method": method,
+                "n_samples": n_samples,
+                "n_features": n_features,
+                "time_mean": time_mean,
+                "time_std": time_std,
+            }
+        )
+
+    params_n_features = [10, 100, 1000]
+    params = product(params_n_features, params_methods)
+    for n_features, method in tqdm(list(params)):
+        n_samples = 1000
+        time_mean, time_std = time_fracdiff(method, n_samples, n_features)
+        results.append(
+            {
+                "method": method,
+                "n_samples": n_samples,
+                "n_features": n_features,
+                "time_mean": time_mean,
+                "time_std": time_std,
+            }
+        )
+
+    pd.DataFrame(results).to_csv("out/results.csv")
